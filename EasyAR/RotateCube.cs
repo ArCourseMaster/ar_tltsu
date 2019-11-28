15f27ffad853a00461a5e26238216a4129a7151d
ArCourseMaster <arcurse_tlt@mail.ru>
2019-11-28 19:46:31 +0400

    Первая фиксация

---
+++ EasyAR/RotateCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Object; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Object.transform.Rotate(Vector3.left);
+    }
+}
